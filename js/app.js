526b616c8019146a06f71f9d7fcb4e325f1cd450
wesley974 <milo974@gmail.com>
2020-04-17 19:05:20 +0400

    initialize a vue instance

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 console.log("%c By Wesley Mouedine Assaby ","background: #F3FFBD; color: #247BA0; font-size: 14px;");
+
+const app = new Vue({
+  el: "h1",
+  data: {
+    title: "Web Boilerplate"
+  }
+})
